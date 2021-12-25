69718657a500794972003a46607bd81b1e7a21ce
Udit Jain <udit@newput.com>
2021-12-25 16:52:53 +0530

    Setting up Express Server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello Server');
+import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const app: Express = express();
+
+app.use(helmet()); // Adds some sensible default security Headers to our app.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('<h1>Hello from the Blockchain Day-7 Assignment Backend!</h1>');
+});
+
+app.listen(PORT, () => console.log(`Running on ${PORT}!!`));
